0b49efe90d19f6f74d1e61b178d470b5d3049095
jovica0707 <58665696+jovica0707@users.noreply.github.com>
2020-08-20 21:41:39 +0200

    Add files via upload

---
+++ Homework 4/Session 5 Homework - 01 Table valued functions.sql
@@ -0,0 +1,31 @@
+/*
+Create multi-statement table value function that for specific
+Teacher and Course will return list of students (FirstName, LastName) 
+who passed the exam, together with Grade and CreatedDate
+
+*/
+	CREATE FUNCTION dbo.fn_TeacherAndCourse
+(
+    @TeacherID INT,
+    @CourseID INT
+)
+RETURNS TABLE
+AS
+RETURN SELECT s.FirstName,
+              s.LastName,
+              g.Grade,
+              g.CreatedDate
+       FROM dbo.Grade g
+           INNER JOIN [dbo].[Student] s
+               ON g.StudentID = s.ID
+       WHERE g.TeacherID = @TeacherID
+             AND g.CourseID = @CourseID;
+
+		SELECT * FROM dbo.fn_TeacherAndCourse (10, 10)
+
+
+
+
+			 
+	
+	
